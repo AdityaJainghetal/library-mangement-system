e6633b4c84b567bd3a87a1b625b325f7eef33780
Aditya Jain <adityajainghetal@gmail.com>
2024-10-29 16:25:02 +0530

    project

---
+++ Delete.js
@@ -0,0 +1,64 @@
+async function deleteData(id) {
+    let api = `http://localhost:3000/DataSave/${id}`;
+    await fetch(api,{
+        method:'DELETE',
+        header:{
+            'Content-Type': 'application/json',
+        }
+    })
+}
+
+async function dataSave()
+{
+ let Table=` <table width="100%" height="100%" border="1px" bgcolor="white" style="font-size: 30px;">
+        <tr bgcolor="orange">
+          <th>Name</th>
+          <th>Edition</th>
+          <th>Author</th>
+          <th>Branch</th>
+          <th>Price</th>
+          <th>Quantity</th>
+          <th>Photo</th>
+          <th>Delted</th>
+        </tr>
+           `
+
+  let api="http://localhost:3000/DataSave";
+
+  let myObj= await fetch(api);
+  let myData= await myObj.json();
+
+   myData.map((key)=>{
+       Table+=`  <tr>
+            <td>${key.Name}</td>
+            <td>${key.edition}</td>
+            <td>${key.Author}</td>
+            <td>${key.Branch}</td>
+            <td>${key.Price}</td>
+            <td>${key.Quantity}</td>
+            <td><img src="${key.Photo}" alt="Book Photo" style="width: 100px; height: auto;"></td>
+            <td>
+            <a href="#" onclick="deleteData('${key.id}')">
+            <img src="del.png" width="30" height="30">
+            </a>
+            </td>
+          </tr> 
+            `
+
+   })
+ Table+="</table>"
+ document.getElementById("demo").innerHTML=Table;
+
+}
+
+dataSave();
+
+// async function deleteData(id) {
+//     let api = `http://localhost:3000/Data/${id}`;
+//     await fetch(api,{
+//         method:'DELETE',
+//         header:{
+//             'Content-Type': 'application/json',
+//         }
+//     })
+// }
